7508d194999ffbf882afe676e20b798a9c900490
Gu Jingxiao <gujingxiao@gmail.com>
2015-10-30 22:08:58 +0800

    + HTTP full header ~ next sleep time function

--- src/main/java/cn/edu/fudan/dianping/crawler/Crawler.java
+++ src/main/java/cn/edu/fudan/dianping/crawler/Crawler.java
@@ -7,7 +7,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * Created by Dawnwords on 2015/10/23.
@@ -23,23 +22,23 @@ public abstract class Crawler {
 
 
     public void crawl(String nextUrl) {
+        String referUrl = null;
         do {
             long timeToSleepMilli = 1000;
             while (true) {
-                System.out.println(nextUrl);
+                System.out.print(nextUrl);
 
                 try {
-                    Response response = connection(nextUrl, Cookie.cookie()).execute();
-
-                    Document document;
-                    if (Cookie.cookie() == null) {
-                        Cookie.cookie(response.cookies());
-                    }
-                    document = response.parse();
+                    Response response = connection(nextUrl, referUrl).execute();
+                    Cookie.cookie(response.cookies());
+                    Document document = response.parse();
                     parseDocument(nextUrl, document);
+                    referUrl = nextUrl;
                     nextUrl = nextUrl(nextUrl, document.select(nextUrlSelector));
+                    System.out.println();
                     break;
                 } catch (IOException e) {
+                    System.out.println('\t' + e.getMessage());
                     Cookie.clearCookie();
                     timeToSleepMilli = sleep(timeToSleepMilli);
                 }
@@ -49,12 +48,21 @@ public abstract class Crawler {
 
     protected abstract void parseDocument(String referURL, Document document);
 
-    private Connection connection(String commentUrl, Map<String, String> cookie) {
+    private Connection connection(String commentUrl, String referUrl) {
         Connection connection = Jsoup.connect(commentUrl)
                 .timeout(30 * 1000)
+                .header("Upgrade-Insecure-Requests", "1")
+                .header("pragma", "no-cache")
+                .header("cache-control", "no-cache")
+                .header("Accept-Language", "en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4")
+                .header("Accept-Encoding", "gzip, deflate, sdch")
+                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
                 .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36");
-        if (cookie != null) {
-            connection = connection.cookies(cookie);
+        if (referUrl != null) {
+            connection.header("Referer", referUrl);
+        }
+        if (Cookie.cookie() != null) {
+            connection = connection.cookies(Cookie.cookie());
         }
         return connection;
     }
@@ -64,7 +72,9 @@ public abstract class Crawler {
             Thread.sleep(time);
         } catch (InterruptedException ignored) {
         }
-        return (long) (time * (2 + Math.random()));
+        long nextSleepTime = (long) (Math.log(time) / Math.log(3) * 20 * (1 + Math.random()));
+        System.out.printf("sleep:%d\t", nextSleepTime);
+        return nextSleepTime;
     }
 
     private String nextUrl(String referURL, Elements nextPage) {
